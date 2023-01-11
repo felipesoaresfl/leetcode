4f06802614c57a4f2e3282523fc6626649eb36ef
Felipe Soares <felipe.soares013@outlook.com>
2023-01-11 18:20:50 -0300

    exercice 136 python easy

---
+++ python/136_single_number.py
@@ -0,0 +1,19 @@
+'''
+Given a non-empty array of integers nums,
+every element appears twice except for one.
+Find that single one.
+
+You must implement a solution with a linear runtime
+complexity and use only constant extra space.
+'''
+
+from collections import Counter
+
+
+class Solution:
+    def single_number(nums):
+        counter_numbers = Counter(nums)
+        for number in nums:
+            if counter_numbers[number] == 1:
+                return number
+    print(single_number([4, 1, 2, 1, 2]))
--- python/1672_richest_customer_wealth.py
+++ python/1672_richest_customer_wealth.py
@@ -18,6 +18,6 @@ Both customers are considered the richest with a wealth of 6 each, so return 6.
 
 
 class Solution:
-    def maximumWealth(accounts):
+    def maximum_wealth(accounts):
         return max(sum(value) for value in accounts)
-    print(maximumWealth([[2, 8, 7], [7, 1, 3], [1, 9, 5]]))
+    print(maximum_wealth([[2, 8, 7], [7, 1, 3], [1, 9, 5]]))
