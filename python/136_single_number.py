2a46ddda3ac187eb3c0d42f40f0c59b6937801bb
Felipe Soares <felipe.soares013@outlook.com>
2023-01-11 18:31:51 -0300

    fix coments exercice 136 python easy

--- python/136_single_number.py
+++ python/136_single_number.py
@@ -5,6 +5,8 @@ Find that single one.
 
 You must implement a solution with a linear runtime
 complexity and use only constant extra space.
+
+https://leetcode.com/problems/single-number/description/
 '''
 
 from collections import Counter
