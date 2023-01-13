0b32258f4ae85e26bff6b1d2a70d39bc6b2ca5ac
Felipe Soares <felipe.soares013@outlook.com>
2023-01-13 09:37:14 -0300

    add exercise 1079 python  medium

---
+++ medium/python/1079_letter_tile_possibilities.py
@@ -0,0 +1,28 @@
+'''
+You have n  tiles, where each tile has one letter tiles[i] printed on it.
+
+Return the number of possible non-empty sequences of letters you can make
+using the letters printed on those tiles.
+
+Input: tiles = "AAB"
+Output: 8
+Explanation: The possible sequences are "A", "B", "AA", "AB", "BA", "AAB",
+"ABA", "BAA".
+
+https://leetcode.com/problems/letter-tile-possibilities/description/
+'''
+
+import itertools
+
+
+class Solution(object):
+    def num_tile_possibilities(tiles):
+        list = []
+        sequences = []
+        for letter in tiles:
+            list.append(letter)
+        for item in range(len(list)+1):
+            for sequence in itertools.permutations(list, item):
+                sequences.append(sequence)
+        return (len(set(sequences)) - 1)
+    print(num_tile_possibilities("AAB"))
